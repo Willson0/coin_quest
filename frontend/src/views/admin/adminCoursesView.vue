9fc0a7e09194af47b6f6d78f6a5a18d849be74f5
Willson0 <logvinovnikita3@gmail.com>
2025-08-24 02:05:05 +0500

    0.2beta

--- frontend/src/views/admin/adminCoursesView.vue
+++ frontend/src/views/admin/adminCoursesView.vue
@@ -107,7 +107,10 @@
                             if (this.selectedLesson[key]) fd.append('count_tries', 2);
                             else fd.append('count_tries', 0);
                         }
-                        else if (JSON.stringify(this.selectedLesson[key]) !== JSON.stringify(oldLesson[key])) fd.append(key, this.selectedLesson[key]);
+                        else if (JSON.stringify(this.selectedLesson[key]) !== JSON.stringify(oldLesson[key])) {
+                            if (typeof this.selectedLesson[key] === 'string') fd.append(key, this.selectedLesson[key]);
+                            else fd.append(key, JSON.stringify(this.selectedLesson[key]));
+                        }
                     }
 
                     axios.post(config.backend + "admin/lessons/" + this.selectedLesson.id, fd, {
