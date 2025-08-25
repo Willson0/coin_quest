d7bba56a06298569e7baf11d4e5d1c137311a2e8
Willson0 <logvinovnikita3@gmail.com>
2025-08-25 18:25:57 +0500

    0.2beta

--- frontend/src/views/admin/adminLoginView.vue
+++ frontend/src/views/admin/adminLoginView.vue
@@ -1,5 +1,6 @@
 <script>
 import config from "@/config.json";
+import {removeLoading} from "@/assets/admin.js";
 export default {
     name: "adminLoginView",
     data () {
@@ -8,8 +9,19 @@ export default {
             password: '',
         }
     },
-    mounted () {
+    async mounted() {
         document.body.classList.add("no-scroll");
+
+        this.styleTag = document.createElement('link');
+        this.styleTag.rel = 'stylesheet';
+        this.styleTag.href = new URL('@/assets/admin.css', import.meta.url).href;
+        document.head.appendChild(this.styleTag);
+
+        document.body.style.backgroundColor = "#12121c";
+
+        this.styleTag.onload = () => {
+            console.log('CSS загружен!');
+        };
     },
     methods: {
         login() {
